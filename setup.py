0cb647199d395b34c06524cfc5908b639f2e2727
RDXRUD <rudrakshagarwal03@gmail.com>
2025-05-12 14:55:14 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ruraksh Agarwal",
+    author_email="rudrakshagarwak03@gmail.com",
+    packages=find_packages()
+)
